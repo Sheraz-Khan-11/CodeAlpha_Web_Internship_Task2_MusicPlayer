72fca0e10cf9ef44359d61e182a651f84e26ad8d
Sheraz-Khan-11 <sherazkhan94.70@gmail.com>
2024-08-06 00:30:04 +0500

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+    {
+        name:"Harley Bird - Home1",
+        artist:"Jordan Schor1",
+        img:"music-1",
+        src:"music-1"
+    },
+    {
+        name:"Harley Bird - Home2",
+        artist:"Jordan Schor2",
+        img:"music-2",
+        src:"music-2"
+    },
+    {
+        name:"Harley Bird - Home3",
+        artist:"Jordan Schor3",
+        img:"music-3",
+        src:"music-3"
+    },
+    {
+        name:"Harley Bird - Home4",
+        artist:"Jordan Schor4",
+        img:"music-4",
+        src:"music-4"
+    },
+    {
+        name:"Harley Bird - Home5",
+        artist:"Jordan Schor5",
+        img:"music-5",
+        src:"music-5"
+    },
+    {
+        name:"Harley Bird - Home6",
+        artist:"Jordan Schor6",
+        img:"music-6",
+        src:"music-6"
+    },
+]
